1212e5a7126ef3abee61005fa24a28b77fac1c3d
WUPENGPENG <WUPENGPENG@kingsoft.com>
2019-08-13 10:41:28 +0800

    update

--- pkg/premonitor/premonitor.go
+++ pkg/premonitor/premonitor.go
@@ -20,14 +20,14 @@ type Premonitor struct {
 func (p *Premonitor) StartPremonitor() {
 	klog.Info("start premonitor... ")
 	historyItems := history.GetHistoryItems(preItemsFile)
-	
-	defer func(){
+
+	defer func() {
 		history.UpdateHistoryItems(historyItems)
 		if err := history.WriteHistoryItems(historyItems, preItemsFile); err != nil {
 			klog.Errorf("Error in write %s", preItemsFile)
 		}
 	}()
-	
+
 	token, err := utils.GetToken(&p.Config.Auth)
 	result, err := utils.GetItems(token, p.Config.Fanli.Premonitor.Url)
 	if err != nil {
@@ -36,18 +36,18 @@ func (p *Premonitor) StartPremonitor() {
 	}
 	if result.Count > 0 {
 		klog.Info("There found some premonitor items !")
-		
+
 		diffItems := utils.GetDiffItems(historyItems, result.Data)
 		if len(diffItems) == 0 {
 			klog.Error("The premonitor items are modify, But there is no new items !")
 			return
 		}
-		
+
 		// only send first item msg, so mark all diffItems to historyItems
 		for _, i := range diffItems {
 			historyItems = append(historyItems, i)
 		}
-		
+
 		tmpfile, err := utils.SaveImage(diffItems[0].GoodsImageUrl)
 		if err != nil {
 			klog.Errorf("Error in download image %s", err)
@@ -61,7 +61,7 @@ func (p *Premonitor) StartPremonitor() {
 			} else {
 				klog.Infof("Success on send image to user %s ", u.Name)
 			}
-			
+
 			msg := utils.GetMsg(diffItems[0], u.Link)
 			klog.V(3).Infof("msg is : %s", msg)
 			if err := utils.SendMessage(msg, u.Name); err != nil {
